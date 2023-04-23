05457681f111817675e535c95f794eb2a5a962b9
Jasmine <jasmineenams@gmail.com>
2023-04-23 09:37:53 -0700

    task 0

--- getppid.c
+++ getppid.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 /**
  * main - print ppid
+ * Return: 0
  */
+int main(void)
+{
+	int i =9;
+	int j = 6;
+	pid_t ppid;
+	int sum = i + j;
+
+	printf("sum is %d\n", sum);
+	ppid = getppid();
+	printf("ppid is %u\n", ppid);
+
+	return (0);
+}
