27c67ef5f3894a5827bbad55655c78d669512fab
Nzivohelsie <kavulinzivo@gmail.com>
2023-04-23 10:32:54 -0700

    task 1

--- prompt.c
+++ prompt.c
@@ -1,23 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 /**
  * main - entry point
  * Return: 0
  */
 int main(void)
 {
+	pid_t child_pid;
 	char *prompt = "simple_shell ";
 	char *line;
+	char **argv;
 	size_t k = 0;
 	ssize_t num_read;
 
-	printf("%s", prompt);
+	while (1)
+	{
+		printf("%s", prompt);
 
-	getline(&line, &k, stdin);
-	printf("%s\n", line);
-
+		num_read = getline(&line, &k, stdin);
+		if (num_read == -1)
+			return (-1);
+		printf("%s\n", line);
+		argv = &line;
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("Error:");
+			return (1);
+		}
+		else if (child_pid == 0)
+		{
+			if (execve(argv[0], argv, NULL) == -1)
+			{
+				perror("Error:");
+			}
+		}
+		else
+			wait (NULL);
+	}
 	free(line);
 	return (0);
-
 }
