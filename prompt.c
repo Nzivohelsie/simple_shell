1830281dc239f8e2d0b63bff04517713ea10f746
Nzivohelsie <kavulinzivo@gmail.com>
2023-04-24 05:57:34 -0700

    memory fixes

--- prompt.c
+++ prompt.c
@@ -7,29 +7,27 @@ int main(void)
 {
 	pid_t child_pid;
 	char *prompt = "simple_shell ";
-	char *line;
-	char **arguments;
-	size_t k;
+	char *line = NULL;
+	char **arguments = NULL;
+	size_t k = 0;
 	ssize_t num_read;
 
 	while (1)
 	{
 		printf("%s", prompt);
-
-		line = malloc(k * sizeof(char));
 		num_read = getline(&line, &k, stdin);
 		if (num_read == -1)
 		{
+			free(line);
 			printf("\n");
 			return (-1);
 		}
-
 		arguments = _strtok(line);
-
 		child_pid = fork();
 		if (child_pid == -1)
 		{
-			perror("Error:");
+			free(line);
+			free(arguments);
 			return (1);
 		}
 		else if (child_pid == 0)
@@ -37,7 +35,11 @@ int main(void)
 			_execve(arguments);
 		}
 		else
+		{
 			wait (NULL);
+			free(*arguments);
+			free(arguments);
+		}
 	}
 	free(arguments);
 	free(line);
--- splitting.c
+++ splitting.c
@@ -7,28 +7,35 @@
  */
 char **_strtok(char *str)
 {
-	char *str_copy;
-	char *token;
-	char **argv;
+	char *str_copy = NULL;
+	char *token = NULL;
+	char **argv = NULL;
 	int strings = 0;
 	int i;
+	char *delimiter = " \n";
+	char *pointer = str;
 
-	str_copy = malloc(strlen(str) * sizeof(char));
-	strcpy(str_copy, str);
-	token = strtok(str, " \n");
+	str_copy = malloc((strlen(pointer) + 1) * sizeof(char));
+	strcpy(str_copy, pointer);
+	token = strtok(pointer, delimiter);
 	while (token != NULL)
 	{
 		strings++;
-		token = strtok(NULL, " \n");
+		token = strtok(NULL, delimiter);
 	}
 	strings++;
 	argv = malloc(strings * sizeof(char *));
-	token = strtok(str_copy, " \n");
+	if (argv == NULL)
+	{
+		free(str_copy);
+		return (NULL);
+	}
+	token = strtok(str_copy, delimiter);
 	for (i = 0; token != NULL; i++)
 	{
-		argv[i] = malloc(strlen(token) * sizeof(char));
+		argv[i] = malloc((strlen(token) + 1) * sizeof(char));
 		strcpy(argv[i], token);
-		token = strtok(NULL, " \n");
+		token = strtok(NULL, delimiter);
 	}
 	argv[i] = NULL;
 	free(str_copy);
