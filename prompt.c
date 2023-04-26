37e07edacad08065109169efd3e901f8620d8dda
Jasmine <jasmineenams@gmail.com>
2023-04-26 09:12:37 -0700

    prompt func

--- prompt.c
+++ prompt.c
@@ -1,61 +1,21 @@
 #include "main.h"
 /**
  * main - entry point
+ * @ac: parameter
+ * @av: argument
+ * @env: parameter
  * Return: 0
  */
-int main(void)
+int main(int ac, char **av, char **env)
 {
-	char *prompt = "simple_shell ";
-	char *line = NULL;
+	char *prompt = "simple_shell ", *line = NULL, *path = NULL;
 	char **arguments = NULL;
 	size_t k = 0;
 	ssize_t num_read;
-	char *path = NULL;
-	int i;
 
 	while (1)
 	{
-		printf("%s", prompt);
+		_printf("%s", prompt);
 		num_read = getline(&line, &k, stdin);
 		if (num_read == -1)
 		{
-			free(line);
-			printf("\n");
-			return (-1);
-		}
-		arguments = _strtok(line);
-		if (*arguments != NULL)
-		{
-			if (_strcmp(arguments[0], "exit") == 0)
-			{
-				free(*arguments);
-				free(arguments);
-				free(line);
-				exit(0);
-			}
-			else if (_strcmp(arguments[0], "env") == 0)
-				_environ();
-		}
-		path = _path(arguments[0]);
-		if (path != NULL)
-		{
-			_execve(path, arguments);
-			free(path);
-			for (i = 0; arguments[i] != NULL; i++)
-			{
-				free(arguments[i]);
-			}
-			free(arguments);
-		}
-		else
-		{
-			free(*arguments);
-			free(arguments);
-			free(path);
-		}
-	}
-	free(*arguments);
-	free(arguments);
-	free(line);
-	return (0);
-}
