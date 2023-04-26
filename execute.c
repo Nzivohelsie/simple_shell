636aa9d1b90622b04a91bb7577b7f8ade2eedefa
Jasmine <jasmineenams@gmail.com>
2023-04-26 08:19:45 -0700

    file to execute commands

---
+++ execute.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _execute - function that executes commands
+ * @path: parameter
+ * @arguments: parameter
+ * @env: parameter
+ * Return: 1
+ */
+int _execute(char *path, char **arguments, char **env)
+{
+		_execve(path, arguments, env);
+		free(path);
+		_free(arguments);
+	return (1);
+}
