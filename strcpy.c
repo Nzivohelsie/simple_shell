bb58f1477a4c8d520decb4269edc405ef6424ec2
Jasmine <jasmineenams@gmail.com>
2023-04-26 09:25:36 -0700

    string copy file

--- strcpy.c
+++ strcpy.c
@@ -10,15 +10,3 @@ char *_strcpy(char *dest, char *src)
 {
 	int le = 0;
 	int i;
-	char *p = dest;
-
-	while (*(src + le) != '\0')
-	{
-		le++;
-	}
-	for (i = 0; i <= le; i++)
-	{
-		dest[i] = src[i];
-	}
-	return (p);
-}
