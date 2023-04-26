d235dfaa4e9079a3b223fc9176c0907100f306a6
Jasmine <jasmineenams@gmail.com>
2023-04-26 09:32:29 -0700

    width func

---
+++ width_function.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_width - calculates the width for printing
+ * @format: formatted string in which to print the arguments.
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
+ *
+ * Return: width.
+ */
+
+int get_width(const char *format, int *i, va_list list)
+{
+	int curr_i;
+	int width = 0;
+
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	{
