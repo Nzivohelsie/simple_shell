daf1096dc7a3afc2e42631516b92870b920f51db
Jasmine <jasmineenams@gmail.com>
2023-04-26 09:09:11 -0700

    path func

--- path.c
+++ path.c
@@ -1,54 +1,11 @@
 #include "main.h"
 /**
- * _path - function that handles the path command
+ * _path - function that handles the path
  * @command: character
  * Return: NULL
  */
 char *_path(char *command)
 {
+	int path_length = 0, command_length = 0;
+	char *path_copy = NULL, *command_path = NULL;
 	char *paths = NULL;
-	int path_length = 0;
-	char *path_copy = NULL;
-	int command_length = 0;
-	char *delimiter = ":";
-	char *token = NULL;
-	char *command_path = NULL;
-	struct stat statbuf;
-
-	if (command == NULL)
-		return (NULL);
-	paths = getenv("PATH");
-	path_length = _strlen(paths);
-	path_copy = malloc((path_length + 1) * sizeof(char));
-	_strcpy(path_copy, paths);
-	command_length = _strlen(command);
-	token = strtok(path_copy, delimiter);
-	while (token != NULL)
-	{
-		command_path = malloc(_strlen(token) + command_length + 2);
-		_strcpy(command_path, token);
-		_strcat(command_path, "/");
-		_strcat(command_path, command);
-		_strcat(command_path, "\0");
-
-		if (stat(command_path, &statbuf) != 0)
-		{
-			free(command_path);
-			command_path = NULL;
-		}
-		else
-		{
-			free(path_copy);
-			return (command_path);
-		}
-		token = strtok(NULL, delimiter);
-	}
-	free(path_copy);
-	if (stat(command, &statbuf) == 0)
-	{
-		command_path = malloc(command_length + 1);
-		command_path = _strcpy(command_path, command);
-                return (command_path);
-	}
-	return (NULL);
-}
